aa1867293d32830cd1df8700613821d6682f65a1
Peyman Abdi <lord.akinak@gmail.com>
2013-08-12 23:04:12 +0430

    new dialog with multiply inputs

--- src/com/nemogames/NativeUIAgent.java
+++ src/com/nemogames/NativeUIAgent.java
@@ -1,6 +1,7 @@
 package com.nemogames;
 
-import org.json.JSONArray;
+import java.util.ArrayList;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,10 +9,16 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.text.Html;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.unity3d.player.UnityPlayer;
@@ -29,7 +36,8 @@ public class NativeUIAgent
 	{
 		MessageBoxButton(1),
 		InputBoxButton(2),
-		PopupMenuButton(3);
+		PopupMenuButton(3),
+		MultiplyInputs(4);
 		int value;
 		NativeUIEvent(int val) { value = val; }
 		public int getValue() { return value; }
@@ -76,7 +84,7 @@ public class NativeUIAgent
 						try 
 						{
 							obj.put("eid", NativeUIEvent.MessageBoxButton.getValue());
-							obj.put("button_index", 1);
+							obj.put("button_index", 0);
 						} catch (JSONException e) {} finally
 						{
 							SendListenerEvent(obj.toString());
@@ -93,7 +101,7 @@ public class NativeUIAgent
 						try 
 						{
 							obj.put("eid", NativeUIEvent.MessageBoxButton.getValue());
-							obj.put("button_index", 2);
+							obj.put("button_index", 1);
 						} catch (JSONException e) {} finally
 						{
 							SendListenerEvent(obj.toString());
@@ -110,7 +118,7 @@ public class NativeUIAgent
 						try 
 						{
 							obj.put("eid", NativeUIEvent.MessageBoxButton.getValue());
-							obj.put("button_index", 3);
+							obj.put("button_index", 2);
 						} catch (JSONException e) {} finally
 						{
 							SendListenerEvent(obj.toString());
@@ -134,20 +142,95 @@ public class NativeUIAgent
 			}
 		});
 	}
+	public void		ShowCustomInputDialog(final String title, final ArrayList inputplaceholders, final String button1, final String button2, final String button3)
+	{
+		RootActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				AlertDialog.Builder dialog = new AlertDialog.Builder(RootActivity);
+				dialog.setTitle(title);
+				
+				LinearLayout ll = new LinearLayout(RootActivity);
+				ll.setOrientation(LinearLayout.VERTICAL);
+				final EditText[] texts = new EditText[inputplaceholders.size()];
+				for (int i = 0; i < inputplaceholders.size(); i++)
+				{
+					String placeholder = (String) inputplaceholders.get(i);
+					texts[i] = new EditText(RootActivity);
+					texts[i].setHint(placeholder);
+					ll.addView(texts[i], new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+				}
+				dialog.setView(ll);
+				dialog.setCancelable(false);
+				if (button1 != "")
+					dialog.setPositiveButton(button1, new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) 
+						{
+							arg0.dismiss();
+							JSONObject obj = new JSONObject();
+							try
+							{
+								obj.put("eid", NativeUIEvent.MultiplyInputs.getValue());
+								for (int i = 0; i < inputplaceholders.size(); i++)
+									obj.put("string_"+i, texts[i].getText().toString());
+								obj.put("button_index", 0);
+							} catch (JSONException e) {} finally
+							{
+								SendListenerEvent(obj.toString());
+							}
+						}
+					});
+				if (button2 != "")
+					dialog.setNeutralButton(button2, new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) 
+						{
+							arg0.dismiss();
+							JSONObject obj = new JSONObject();
+							try 
+							{
+								obj.put("eid", NativeUIEvent.MultiplyInputs.getValue());
+								for (int i = 0; i < inputplaceholders.size(); i++)
+									obj.put("string_"+i, texts[i].getText().toString());
+								obj.put("button_index", 1);
+							} catch (JSONException e) {} finally
+							{
+								SendListenerEvent(obj.toString());
+							}
+						}
+					});
+				if (button3 != "")
+					dialog.setNegativeButton(button3, new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) 
+						{
+							arg0.dismiss();
+							JSONObject obj = new JSONObject();
+							try 
+							{
+								obj.put("eid", NativeUIEvent.MultiplyInputs.getValue());
+								for (int i = 0; i < inputplaceholders.size(); i++)
+									obj.put("string_"+i, texts[i].getText().toString());
+								obj.put("button_index", 2);
+							} catch (JSONException e) {} finally
+							{
+								SendListenerEvent(obj.toString());
+							}
+						}
+					});
+				dialog.show();
+			}			
+		});
+	}
 	
 	public void		ShowProgressDialog(final String title, final String message, final boolean indicator)
 	{
-		if (last_dialog != null && last_dialog.isShowing())
-		{
-			Log.w("Nemo - NativeUIAgent", "A dialog is already open, cannot create new one!");
-			return;
-		}
-		if (still_waiting)
-		{
-			Log.w("Nemo - NativeUIAgent", "A progress is still waiting to finish.");
-			return;
-		}
-		
 		still_waiting = true;
 		RootActivity.runOnUiThread(new Runnable()
 		{
@@ -180,7 +263,7 @@ public class NativeUIAgent
 					{
 						progressDialog.dismiss();
 					}
-				};
+				}.execute(0);
 			}
 		});
 	}
@@ -188,18 +271,6 @@ public class NativeUIAgent
 	public void		CloseBusy()
 	{
 		still_waiting = false;
-	}
-	
-	public void		TakeScreenshot(final String path)
-	{
-		RootActivity.runOnUiThread(new Runnable()
-		{
-			@Override
-			public void run() 
-			{
-				
-			}
-		});
 	}
 	
 	public void		ShowWebView(final String title, final String url, final int width, final int height)
@@ -238,7 +309,8 @@ public class NativeUIAgent
 		});
 	}
 	
-	public void		ShowMessageBoxWithInput(final String title, final String placeholder, final String button1, final String button2)
+	public void		ShowMessageBoxWithInput(final String title, final String message,
+			final String button1, final String button2, final String button3)
 	{
 		if (last_dialog != null && last_dialog.isShowing())
 		{
@@ -251,42 +323,114 @@ public class NativeUIAgent
 			@Override
 			public void run() 
 			{
-				
+				final EditText txt = new EditText(RootActivity);
+				AlertDialog.Builder dialog = new AlertDialog.Builder(RootActivity);
+				dialog.setTitle(title);
+				dialog.setMessage(message);
+				txt.setText("");
+				dialog.setView(txt);
+				dialog.setCancelable(false);
+				if (!button1.equals("")) dialog.setPositiveButton(button1, new DialogInterface.OnClickListener()
+				{
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) 
+					{
+						arg0.dismiss();
+						JSONObject obj = new JSONObject();
+						try 
+						{
+							obj.put("eid", NativeUIEvent.InputBoxButton.getValue());
+							obj.put("button_index", 0);
+							obj.put("input", txt.getText());
+						} catch (JSONException e) {} finally
+						{
+							SendListenerEvent(obj.toString());
+						}
+					}
+				});
+				if (!button2.equals("")) dialog.setNeutralButton(button2, new DialogInterface.OnClickListener()
+				{
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) 
+					{
+						arg0.dismiss();
+						JSONObject obj = new JSONObject();
+						try 
+						{
+							obj.put("eid", NativeUIEvent.InputBoxButton.getValue());
+							obj.put("button_index", 1);
+							obj.put("input", txt.getText());
+						} catch (JSONException e) {} finally
+						{
+							SendListenerEvent(obj.toString());
+						}
+					}
+				});
+				if (!button3.equals("")) dialog.setNegativeButton(button3, new DialogInterface.OnClickListener()
+				{
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) 
+					{
+						arg0.dismiss();
+						JSONObject obj = new JSONObject();
+						try 
+						{
+							obj.put("eid", NativeUIEvent.InputBoxButton.getValue());
+							obj.put("button_index", 2);
+							obj.put("input", txt.getText());
+						} catch (JSONException e) {} finally
+						{
+							SendListenerEvent(obj.toString());
+						}
+					}
+				});
+				dialog.show();
 			}
 		});
 	}
 	
-	public void		ShowPopupMenu(final String jsonmenu)
+	public void		ShowPopupMenu(final String[] buttons)
 	{
 		RootActivity.runOnUiThread(new Runnable()
 		{
 			@Override
 			public void run() 
 			{
-				CharSequence[] items;
-				JSONArray array = null;
-				try {
-					array = new JSONArray(jsonmenu);
-					items = new CharSequence[array.length()];
-					for (int i = 0; i < array.length(); i++) items[i] = array.getString(i);
-					AlertDialog.Builder ad = new AlertDialog.Builder(RootActivity);
-					ad.setItems(items, new DialogInterface.OnClickListener()
-				    {
-				        @Override
-				        public void onClick(DialogInterface dialog, int which)
-				        {
-				        	JSONObject obj = new JSONObject();
-				        	try {
-				        		obj.put("eid", NativeUIEvent.PopupMenuButton.getValue());
-								obj.put("button_name", which+1);
-							} catch (JSONException e) { e.printStackTrace(); } finally
-							{
-								SendListenerEvent(obj.toString());
-							}
-				        } 
-				    });
-					ad.show();
-				} catch (JSONException e) { e.printStackTrace(); }
+				AlertDialog.Builder ad = new AlertDialog.Builder(RootActivity);
+				ad.setItems(buttons, new DialogInterface.OnClickListener()
+			    {
+			        @Override
+			        public void onClick(DialogInterface dialog, int which)
+			        {
+			        	JSONObject obj = new JSONObject();
+			        	try {
+			        		obj.put("eid", NativeUIEvent.PopupMenuButton.getValue());
+							obj.put("button_index", which);
+						} catch (JSONException e) { e.printStackTrace(); } finally
+						{
+							SendListenerEvent(obj.toString());
+						}
+			        } 
+			    });
+				ad.show();
+			}
+		});
+	}
+	
+	public void		ShowComposeMail(final String address, final String subject, final String htmlBody, final String attachment)
+	{
+		RootActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				Intent intent = new Intent(Intent.ACTION_SEND);
+				intent.setType("text/html");
+				intent.putExtra(Intent.EXTRA_EMAIL, new String[]{address});
+				intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+				intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(htmlBody));
+				if (!attachment.equals("")) intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + attachment));
+				RootActivity.startActivity(intent);
 			}
 		});
 	}
